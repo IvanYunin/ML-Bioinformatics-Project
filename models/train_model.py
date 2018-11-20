e3da0712e23f0348b2ea51d64ca735eef494c758
IvanYunin <79103889939@yandex.ru>
2018-11-20 21:03:48 +0300

    add main scripts

--- models/train_model.py
+++ models/train_model.py
@@ -1,7 +1,9 @@
 import mxnet as mx
 import pickle
 import logging
-import my_nn 
+import my_nn
+import argparse
+import sys
 
 def save_obj(obj, name ):
     with open(name + '.pkl', 'wb') as f:
@@ -11,33 +13,10 @@ def save_obj(obj, name ):
 def load_obj(name ):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
-    
-
-def data_10_classes(data):
-    train_label_new =  data['train_label']
-    test_label_new = data['test_label']
-    
-    for idx in range(len(train_label_new)):
-        if train_label_new[idx] <= 100:
-            train_label_new[idx] = int(train_label_new[idx] / 10)
-        else:
-            train_label_new[idx] = 9
-    
-    for idx in range(len(test_label_new)):
-        if test_label_new[idx] <= 100:
-            test_label_new[idx] = int(test_label_new[idx] / 10)
-        else:
-            test_label_new[idx] = 9
-            
-    train_iter = mx.io.NDArrayIter(data['train_data'], train_label_new,
-                               batch_size, shuffle = True)
-    val_iter = mx.io.NDArrayIter(data['test_data'],
-                             test_label_new, batch_size)
-    return [train_iter, val_iter]
-
-    
+       
     
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser()
     parser.add_argument('-data', '--data', help = 'data file name',
         default = 'gse40249_data')
@@ -47,46 +26,62 @@ if __name__ == '__main__':
         type = float, default = 0.1)
     parser.add_argument('-ne', '--num_epoch', help = 'epoch count',
         type = int, default = 1)
-    parser.add_argument('-m_n', '--model_name', help = 'modelname', default = 'fcnn_class_age')
-  
-    
+    parser.add_argument('-r', '--ratio', help = 'train/val datasets ratio',
+        type = float, default = 0.8)
+    parser.add_argument('-model_name', '--model_name', 
+                        help = 'model name Available: fcnn_class_age, fcnn_10_classes, log_reg',
+                        default = 'fcnn_class_age')
+
     args = parser.parse_args()
-    
+    logging.getLogger().setLevel(logging.DEBUG)
+    model = mx.mod.BaseModule()
     print('Loading train/val data and Constructing network model')
     gse_data = load_obj("../../data/transform_data/"+args.data)
     
     if (args.model_name == 'fcnn_class_age'):
         train_iter = mx.io.NDArrayIter(gse_data['train_data'],
                                        gse_data['train_label'],
-                               args.batch_size, shuffle = True)
+                                       args.batch_size, shuffle = True)
         val_iter = mx.io.NDArrayIter(gse_data['test_data'],
-                             gse_data['test_label'], args.batch_size)
+                                     gse_data['test_label'], args.batch_size)
         model = my_nn.create_fcnn(74)
         
     elif (args.model_name == 'fcnn_10_classes'):
-        [train_iter, val_iter] = data_10_classes(gse_data)
+        [train_iter, val_iter] = my_nn.data_10_classes(gse_data, 
+                                                       args.batch_size)
         model = my_nn.create_fcnn(10)
         
+    elif (args.model_name == 'log_reg'):
+        train_iter = mx.io.NDArrayIter(gse_data['train_data'],
+                                       gse_data['train_label'],
+                                       args.batch_size, shuffle = True, 
+                                       label_name='label')
+        val_iter = mx.io.NDArrayIter(gse_data['test_data'],
+                                     gse_data['test_label'], args.batch_size)
+        model = my_nn.create_log_reg_fcnn()
     else:
-        print('Error, check arguments')
+        print('Error(model name), check arguments. Model name Available: fcnn_class_age, fcnn_10_classes, log_reg')
         sys.exit(0)
+    print('Loading train/val data and Constructing network model DONE!')
     
+   #Some errors on Cross validation
+   #print('Cross validation')
+   #my_nn.cross_val(model, gse_data, args.batch_size, args.learning_rate, 
+   #                args.num_epoch, args.ratio)
+   #print('Cross validation DONE!')
     print('Training network')
     accuracy = mx.metric.Accuracy()
-    ce_loss = mx.metric.CrossEntropy()
-    model.fit(train_iter, eval_data = val_iter, optimizer = 'sgd',
-        optimizer_params = {'learning_rate': args.learning_rate},
-        eval_metric = mx.metric.CompositeEvalMetric([accuracy, ce_loss]),
-        batch_end_callback = mx.callback.Speedometer(args.batch_size,100),
-        num_epoch = args.num_epoch)
-
-    print('Testing network')
     
-    predictions = fc_model.predict(val_iter)
-    print('Predictions shape: {}'.format(predictions.shape))
-    metric = mx.metric.Accuracy()
-    fc_model.score(val_iter, metric)
-    print('Accuracy: {}'.format(metric))
+    model.fit(train_iter,eval_data = val_iter,optimizer = 'sgd',
+              optimizer_params = {'learning_rate':args.learning_rate},
+              eval_metric = 'acc',
+              batch_end_callback = mx.callback.Speedometer(args.batch_size, 100),
+              num_epoch = args.num_epoch)
+    print('Training network DONE!')
+    print('Testing network')
+    model.score(val_iter, accuracy)
+    print('Accuracy: {}'.format(accuracy))
+    print('Testing network DONE')
                 
     
                 
